e07a81709d26efe96a7a125cfea9e9fdc94c1680
Luis Antonio <luisscudeler@yahoo.com.br>
2023-02-20 19:19:32 -0300

    Implements other operators

--- src/main/java/com/scud/project/MathController.java
+++ src/main/java/com/scud/project/MathController.java
@@ -19,6 +19,47 @@ public class MathController {
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
 
+    @RequestMapping(value="/sub/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double sub(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Error input");
+        }
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value="/prod/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double prod(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Error input");
+        }
+        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value="/div/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double div(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Error input");
+        }
+        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value="/med/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double med(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Error input");
+        }
+        return (convertToDouble(numberOne) + convertToDouble(numberTwo))/2;
+    }
+
+    @RequestMapping(value="/squa/{numberOne}}", method = RequestMethod.GET)
+    public Double squa(@PathVariable(value = "numberOne") String numberOne) {
+        if (!isNumeric(numberOne)) {
+            throw new UnsupportedMathOperationException("Error input");
+        }
+        return Math.sqrt(convertToDouble(numberOne));
+    }
+
+
     private Double convertToDouble(String strNumber) {
         if(strNumber == null) return 0D;
         String number = strNumber.replaceAll(",",".");
