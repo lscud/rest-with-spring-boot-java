8026e232d94e38a923ef54f53ceef8bf63b3bab9
Luis Antonio <luisscudeler@yahoo.com.br>
2023-02-20 14:32:19 -0300

    Initial commit

---
+++ src/test/java/com/scud/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scud.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
